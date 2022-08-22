56b115449ef1a34c350d691b2d280cbe1a0d5f00
qud0511 <qkrqudwnsqkrqudwns0511@gmail.com>
2022-08-22 11:31:49 +0900

    Update example.py

--- example.py
+++ example.py
@@ -1,7 +1,20 @@
-N = int(input())
-S = [int(input() for _ in range(N))]
-# list comprehension
-print(S)
+# BOJ 2018 문제
+from collections import Counter
 
+N = int(input())
+
+S = [int(input() for _ in range(N))] # list comprehension
 # for i in range(N):
 #     S.append(int(input()))
+
+S.sort()
+
+# print(sum(S)//N)
+# print(S[len(2)//2])
+# print(Counter(S).most_common())
+# print(max(S)-min(S))
+
+print(sum(S)//N)
+print(S[N(2)//2])
+print(Counter(S).most_common())
+print(max(S)-min(S))
